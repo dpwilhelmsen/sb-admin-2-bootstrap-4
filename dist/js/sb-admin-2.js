fca3c5655bda96c1763843424977373ccbefb735
Daniel Wilhelmsen <dpwilhelmsen@yahoo.com>
2017-09-12 14:35:45 -0700

    Update repo and index to use Bootstrap 4

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -1,6 +1,6 @@
 /*!
- * Start Bootstrap - SB Admin 2 v3.3.7+1 (http://startbootstrap.com/template-overviews/sb-admin-2)
- * Copyright 2013-2016 Start Bootstrap
+ * Start Bootstrap - SB Admin 2 Bootstrap 4 v4.0.0 (http://startbootstrap.com/template-overviews/sb-admin-2)
+ * Copyright 2013-2017 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
  */
 $(function() {
