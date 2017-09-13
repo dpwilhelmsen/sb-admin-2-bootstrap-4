2e7fa3d17328f75d1635444dff34da9f10db1368
Daniel Wilhelmsen <dpwilhelmsen@yahoo.com>
2017-09-13 10:02:53 -0700

    Switch from less to sass

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -1,7 +1,7 @@
 /*!
- * Start Bootstrap - SB Admin 2 Bootstrap 4 v4.0.0 (http://startbootstrap.com/template-overviews/sb-admin-2)
- * Copyright 2013-2017 Start Bootstrap
- * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
+ * Start Bootstrap - SB Admin 2 for Bootstrap 4 v4.0.0 (http://github.com/dpwilhelmsen/sb-admin-2-bootstrap-4)
+ * Copyright 2013-2017 Daniel Wilhelmsen
+ * Licensed under MIT (https://github.com/dpwilhelmsen/sb-admin-2-bootstrap-4/blob/master/LICENSE)
  */
 $(function() {
     $('#side-menu').metisMenu();
@@ -9,20 +9,27 @@ $(function() {
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
 $(function() {
     $(window).bind("load resize", function() {
+        var topOffset = 50;
         var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
             $('div.navbar-collapse').removeClass('show');
+            topOffset = 100; // 2-row-menu
         } else {
             $('div.navbar-collapse').addClass('show');
+        }
+
+        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
         }
     });
 
     var url = window.location;
-    // var element = $('ul.nav a').filter(function() {
-    //     return this.href == url;
-    // }).addClass('active').parent().parent().addClass('in').parent();
     var element = $('ul.nav a').filter(function() {
         return this.href == url;
     }).addClass('active').parent();
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
@@ -16,19 +16,18 @@ var banner = ['/*!\n',
     ''
 ].join('');
 
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('less/sb-admin-2.less')
-        .pipe(less())
+gulp.task('sass', function() {
+    return gulp.src('scss/sb-admin-2.scss')
+        .pipe(sass())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
-});
+})
 
 // Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
+gulp.task('minify-css', ['sass'], function() {
     return gulp.src('dist/css/sb-admin-2.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
@@ -65,7 +64,7 @@ gulp.task('copy', function() {
     gulp.src(['bower_components/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
         .pipe(gulp.dest('vendor/bootstrap'))
 
-    gulp.src(['bower_components/bootstrap-social/*.css', 'bower_components/bootstrap-social/*.less', 'bower_components/bootstrap-social/*.scss'])
+    gulp.src(['bower_components/bootstrap-social/*.css', 'bower_components/bootstrap-social/*.scss'])
         .pipe(gulp.dest('vendor/bootstrap-social'))
 
     gulp.src(['bower_components/datatables/media/**/*'])
@@ -116,8 +115,8 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'js', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'js', 'minify-js'], function() {
+    gulp.watch('scss/*.scss', ['sass']);
     gulp.watch('dist/css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
