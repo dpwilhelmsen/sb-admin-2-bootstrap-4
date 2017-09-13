2dd1ba998d4af217157c71878793d2e4e20677f7
Daniel Wilhelmsen <dpwilhelmsen@yahoo.com>
2017-09-13 10:16:03 -0700

    Add morris line chart example

--- data/morris-data.js
+++ data/morris-data.js
@@ -113,5 +113,19 @@ $(function() {
         hideHover: 'auto',
         resize: true
     });
+
+    new Morris.Line({
+        element: 'morris-line-chart',
+        data: [
+            { year: '2008', value: 20 },
+            { year: '2009', value: 10 },
+            { year: '2010', value: 5 },
+            { year: '2011', value: 5 },
+            { year: '2012', value: 20 }
+        ],
+        xkey: 'year',
+        ykeys: ['value'],
+        labels: ['Value']
+    });
     
 });
