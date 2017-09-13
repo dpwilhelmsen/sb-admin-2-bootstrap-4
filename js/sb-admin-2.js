d6fcf814e9e08303c34021d89708da93432a00fe
Daniel Wilhelmsen <dpwilhelmsen@yahoo.com>
2017-09-13 09:47:12 -0700

    Clean sidebar show hide functionality

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -4,30 +4,17 @@ $(function() {
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
-// Sets the min-height of #page-wrapper to window size
 $(function() {
     $(window).bind("load resize", function() {
-        var topOffset = 50;
         var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
-            $('div.navbar-collapse').addClass('collapse');
-            topOffset = 100; // 2-row-menu
+            $('div.navbar-collapse').removeClass('show');
         } else {
-            $('div.navbar-collapse').removeClass('collapse');
-        }
-
-        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-        height = height - topOffset;
-        if (height < 1) height = 1;
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
+            $('div.navbar-collapse').addClass('show');
         }
     });
 
     var url = window.location;
-    // var element = $('ul.nav a').filter(function() {
-    //     return this.href == url;
-    // }).addClass('active').parent().parent().addClass('in').parent();
     var element = $('ul.nav a').filter(function() {
         return this.href == url;
     }).addClass('active').parent();
